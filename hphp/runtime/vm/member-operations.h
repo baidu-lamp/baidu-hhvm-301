7a6d4df4535df9d7dc9e9b9e6cc7af735bf80864
huzhiguang <huzhiguang@baidu.com>
2016-06-06 04:35:22 +0800

    warning level from warning to info  when object don't exists
    cn:
    对象不存在时将警告级别由warning调到info，加入单测

--- hphp/runtime/vm/member-operations.h
+++ hphp/runtime/vm/member-operations.h
@@ -1690,7 +1690,7 @@ inline DataType propPreNull(TypedValue& tvScratch, TypedValue*& result) {
   tvWriteNull(&tvScratch);
   result = &tvScratch;
   if (warn) {
-    raise_warning("Cannot access property on non-object");
+    raise_notice("Cannot access property on non-object");
   }
   return KindOfNull;
 }
